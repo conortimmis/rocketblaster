16e7081b2652c862a9a02851f7951f7b9c9cc8c8
conortimmis <t1506504@student.southstaffs.ac.uk>
2017-06-09 10:41:30 +0100

    MainMenu Updated

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -1,39 +1,34 @@
-  BasicGame.MainMenu = function (game) { };
- 
-  var startButton;
-  var starfield;
-  var logo;
- 
-  BasicGame.MainMenu.prototype = {
- 
-  create: function () {
+BasicGame.MainMenu = function (game) { };
 
-// We've already preloaded our assets, so let's kick right into
-//the Main Menu itself.
-// Here all we're doing is playing some music and adding a
-//picture and button
-// Naturally I expect you to do something significantly better
-//better :)
+var startButton;
+var starfield;
+var logo;
 
- //Outputting sky, ship, score, lives, total and Start Time to the
+BasicGame.MainMenu.prototype = {
+
+create: function () {
+// We've already preloaded our assets, so let's kick right into the Main Menu itself.
+// Here all we're doing is playing some music and adding a picture and button
+// Naturally I expect you to do something significantly better.
+
+//Outputting sky, ship, score, lives, total and Start Time to the
 screen
- // The scrolling starfield background
- starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
- logo = this.add.sprite((this.world.width / 2), (this.world.height /
-2) - 150, 'logo');
- logo.anchor.setTo(0.5,0.5);
- startButton = this.add.button((this.world.width / 2),
+// The scrolling starfield background
+starfield = this.add.tileSprite(0, 0, 800, 600, 'starfield');
+logo = this.add.sprite((this.world.width / 2), (this.world.height /2) - 150, 'logo');
+logo.anchor.setTo(0.5,0.5);
+startButton = this.add.button((this.world.width / 2),
 (this.world.height / 2) + 50, 'startButton', this.startGame);
- startButton.anchor.setTo(0.5,0.5);
+startButton.anchor.setTo(0.5,0.5);
+},
+
+update: function () {
+// Do some nice funky main menu effect here
  },
 
- update: function () {
- // Do some nice funky main menu effect here
- },
-
- startGame: function () {
- // And start the actual game
+startGame: function () {
+// And start the actual game
 this.game.state.start('Game');
- }
+}
 
- };
+};
